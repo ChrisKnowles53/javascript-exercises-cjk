aef8c5a55dcd193a9e53ecd5bcf5586b7960c568
Chris Knowles <chris.knowles@thinkmoney.co.uk>
2023-10-04 14:45:25 +0100

    forgot to save before pushing last time

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -4,21 +4,18 @@
 // fibanacci is the sum of the 2 preceeding number 0,1,1,2,3,5,7,10
 
 const fibonacci = function (input) {
-  console.log(input);
-  const numberInput = parseInt(`"${input}"`, 10);
-
-  if (numberInput < 0) {
+  if (input < 0) {
     return "please use a number greater than 1";
   } else {
     let fibonacciArray = [0, 1];
-    for (let i = 2; i <= numberInput; i++) {
+    for (let i = 2; i <= input; i++) {
       fibonacciArray.push(fibonacciArray[i - 2] + fibonacciArray[i - 1]);
     }
-    return fibonacciArray[numberInput];
+    return fibonacciArray[input];
   }
 };
 
-console.log(fibonacci("four"));
+console.log(fibonacci(4));
 
 // Do not edit below this line
 module.exports = fibonacci;
