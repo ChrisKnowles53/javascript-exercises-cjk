650dd001d93d58e11704fbab2b8b3618d443e085
Chris Knowles <chris.knowles@thinkmoney.co.uk>
2023-10-04 11:52:54 +0100

    09_palindrome completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
+// my original way
+const palindromesOriginal = function (input) {
+  let splitInput = input.split("");
+  let reversingInput = splitInput.reverse();
+  let reversedInput = reversingInput.join("");
 
+  if (input === reversedInput) return true;
+  else {
+    return false;
+  }
 };
+
+console.log(palindromesOriginal("racecar"));
+
+// the way Odin are teaching
+const palindromesOdin = function (input) {
+  return input.split("").reverse().join("") === input;
+};
+
+console.log(palindromesOdin("racecar"));
 
 // Do not edit below this line
 module.exports = palindromes;
