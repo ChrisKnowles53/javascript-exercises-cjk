869548ae5ab44d7cabacf7f0cad69e6db1176bca
Chris Knowles <chris.knowles@thinkmoney.co.uk>
2023-10-04 15:43:24 +0100

    11 completed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,19 @@
-const getTheTitles = function() {
+const books = [
+  {
+    title: "JSFundamentals",
+    author: "Odin",
+  },
+  {
+    title: "REACT",
+    author: "Drama Queen",
+  },
+];
 
+const getTheTitles = function (array) {
+  return array.map((books) => books.title);
 };
+
+getTheTitles(books);
 
 // Do not edit below this line
 module.exports = getTheTitles;
